fb204d116b4b678ce1fd3f47023d44691e39ba9b
murat <muratdere90@gmail.com>
2017-12-19 15:02:03 +0300

    ilk view

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import include
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls'))
 ]
